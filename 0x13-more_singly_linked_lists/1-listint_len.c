e310afdfc82825abb722e130c9eb8c052c5b34f8
shamzaou <shamzaou@student.42abudhabi.ae>
2023-04-30 16:50:53 +0400

    sub

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,24 +1,3 @@
-<<<<<<< HEAD
-#include"lists.h"
-#include"stdio.h"
-/**
- *listint_len - return length of element
- *
- *@h: argument pass for function
- *
- *Return: size_t
- */
-
-size_t listint_len(const listint_t *h)
-{
-size_t i = 0;
-while (h)
-{
-h = h->next;
-i++;
-}
-return (i);
-=======
 #include "lists.h"
 
 /**
@@ -39,5 +18,4 @@ size_t listint_len(const listint_t *h)
 		tp = tp->next;
 	}
 	return (cnr);
->>>>>>> 350df5726b6cfac0e65feafeaf746eac4dc11f0f
 }
