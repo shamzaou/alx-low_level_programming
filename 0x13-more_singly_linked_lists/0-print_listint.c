c8d1957e063f77d4dbdab5e46bbc752ffb6f5fc5
shamzaou <shamzaou@student.42abudhabi.ae>
2023-04-30 16:46:44 +0400

    sub

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,25 +1,3 @@
-<<<<<<< HEAD
-#include"lists.h"
-#include<stdio.h>
-
-/**
- * print_listint - print elements of linkedlist
- * @h: linked list
- *
- * Return: number of nodes
- */
-
-size_t print_listint(const listint_t *h)
-{
-size_t i = 0;
-while (h)
-{
-printf("%i\n", h->n);
-h = h->next;
-i++;
-}
-return (i);
-=======
 #include "lists.h"
 #include <stdio.h>
 
@@ -42,5 +20,4 @@ size_t print_listint(const listint_t *h)
 		tp = tp->next;
 	}
 	return (cntr);
->>>>>>> 350df5726b6cfac0e65feafeaf746eac4dc11f0f
 }
