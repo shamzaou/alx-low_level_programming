7f0b797087ad6f73e5b19488d46a08fc1a14ed52
shamzaou <shamzaou@student.42abudhabi.ae>
2023-05-04 16:02:52 +0400

    bit manip

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,14 +1,14 @@
 #include "main.h"
 
 /**
- * binary_to_uint - converts a binary number to unsigned int
+ * binary_to_uint - converts a binary number to an unsigned int
  * @b: string containing the binary number
  * Return: the converted number
  */
 unsigned int binary_to_uint(const char *b)
 {
 	int i;
-	unsigned int dec_val = 0;
+	unsigned int decimal = 0;
 
 	if (!b)
 		return (0);
@@ -17,7 +17,7 @@ unsigned int binary_to_uint(const char *b)
 	{
 		if (b[i] < '0' || b[i] > '1')
 			return (0);
-		dec_val = 2 * dec_val + (b[i] - '0');
+		decimal = 2 * decimal + (b[i] - '0');
 	}
-	return (dec_val);
+	return (decimal);
 }
--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * flip_bits - number of bits you would need to flip
+ * flip_bits - number of bits you need to flip
  * to get from one number to another
  * @n: parameter
  * @m: destiny
@@ -9,15 +9,15 @@
  */
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
-	unsigned int i, nflips = 0;
+	unsigned int i, n_flips = 0;
 	unsigned long int j = sizeof(unsigned long int) * 8;
 
 	for (i = 0; i < j; i++)
 	{
 		if ((m & 1) != (n & 1))
-			nflips += 1;
+			n_flips += 1;
 		n = n >> 1;
 		m = m >> 1;
 	}
-	return (nflips);
+	return (n_flips);
 }
